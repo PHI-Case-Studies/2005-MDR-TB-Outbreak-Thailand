383c07b36ef35d3907890b8e12c4b757f477756b
Herman Tolentino <herman.tolentino@gmail.com>
2019-07-05 17:35:45 -0500

    Azure Notebook set up update

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -6,6 +6,7 @@ source /home/nbuser/anaconda3_420/bin/activate
 # Install packages
 conda update -c conda-forge conda conda-build
 conda install -y -c conda-forge folium=0.9.1 jinja2=2.10* xlrd networkx=2.3*
+conda install -c plotly --yes plotly_express
 
 pip install --upgrade pip
 pip install pandas==0.24.2
