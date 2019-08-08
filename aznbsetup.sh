023476c62709b443fd852ee7576e3a08e8431ed7
Herman Tolentino <herman.tolentino@gmail.com>
2019-08-07 21:46:33 -0500

    Updated scripts

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -5,9 +5,9 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.9.1 jinja2=2.10* xlrd networkx=2.3* missingno=0.4* bokeh
+conda install -y -c conda-forge folium=0.9.1 jinja2=2.10* xlrd networkx=2.3* missingno=0.4* bokeh pandas=0.25*
 
 pip install --upgrade pip
-pip install pandas==0.24.2 plotly==4.0.0
+pip install pyviz plotly==4.0.0
 
 source /home/nbuser/anaconda3_420/bin/deactivate
